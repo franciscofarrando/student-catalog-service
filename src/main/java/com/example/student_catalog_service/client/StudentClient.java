4e497a23db65cc15a06ad0b673dc38fa3b8180e6
Francisco Farrando <121203370+franciscofarrando@users.noreply.github.com>
2025-06-01 18:18:56 +0200

    lab 2 ok

---
+++ src/main/java/com/example/student_catalog_service/client/StudentClient.java
@@ -0,0 +1,4 @@
+package com.example.student_catalog_service.client;
+
+public class StudentClient {
+}
---
+++ src/main/java/com/example/student_catalog_service/dto/StudentInfoDTO.java
@@ -0,0 +1,4 @@
+package com.example.student_catalog_service.dto;
+
+public class StudentInfoDTO {
+}
